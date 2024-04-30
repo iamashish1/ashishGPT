0f89b4251dae31b7d51077a6691cd18da7b26093
Ashish <iamashishkoirala1@gmail.com>
2024-04-30 01:25:42 -0400

    designed user interface

--- app/src/main/java/com/example/ashishgpt/Adapter.kt
+++ app/src/main/java/com/example/ashishgpt/Adapter.kt
@@ -6,83 +6,52 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-
 class Adapter(
-    private var rooms:MutableList<ChatClass>,
-): RecyclerView.Adapter<Adapter.RoomViewHolder>() {
+    private var rooms: MutableList<ChatClass>,
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun getItemViewType(position: Int): Int {
-
-//        return super.getItemViewType(position)
-
-
-        if(rooms[position].isSender){
-            return 0;
-        }else{
-            return 1;
+        return if (rooms[position].isSender) {
+            0
+        } else {
+            1
         }
     }
 
-
-    inner class RoomViewHolder(roomView: View): RecyclerView.ViewHolder(roomView){
-        var tvTitle: TextView
-
-        init {
-            tvTitle = roomView.findViewById(R.id.textView)
-
-
-            roomView.setOnClickListener {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-
-                    //NOTE: This click listener can also be set in onBindViewHolder
-                }
-            }
-
-        }
+    inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvTitle: TextView = itemView.findViewById(R.id.userDesc)
+        // Other views specific to UserViewHolder
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoomViewHolder {
-        if(viewType==0){
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.user_item,parent,false)
-            return RoomViewHolder(view)
-        }else{
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.gpt_item,parent,false)
-            return RoomViewHolder(view)
+    inner class GPTViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvTitle: TextView = itemView.findViewById(R.id.gptTitle)
+        // Other views specific to GPTViewHolder
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == 0) {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
+            UserViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.gpt_item, parent, false)
+            GPTViewHolder(view)
         }
-
     }
 
     override fun getItemCount(): Int {
         return rooms.size
     }
 
-    override fun onBindViewHolder(holder: RoomViewHolder, position: Int) {
-//        holder.itemView.findViewById<TextView>(R.id.TitleId).text=rooms[position].title
-//        holder.itemView.findViewById<TextView>(R.id.AddressId).text=rooms[position].address
-        //OR
-//        holder.itemView.apply {
-//            findViewById<TextView>(R.id.TitleId).text=rooms[position].title
-//            findViewById<TextView>(R.id.AddressId).text=rooms[position].address
-//        }
-        // OR
-        // Load image into ImageView using Coil
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val currentRoom = rooms[position]
 
-
-
-        holder.tvTitle.text = rooms[position].question
-//        holder.tvAddress.text = rooms[position].address
-//        holder.tvRent.text = rooms[position].rent
-//        holder.tvUtility.text = if (rooms[position].isUtilityIncluded) {
-//            "Utility Included"
-//        } else {
-//            "Utility Not Included"
-//        }
-//
-//        holder.tvFurnishing.text = rooms[position].furnishingType.toString()
-//        holder.tvHouseType.text = rooms[position].houseType.toString()
-
+        if (holder is UserViewHolder) {
+            holder.tvTitle.text = currentRoom.question
+            // Set other views specific to UserViewHolder
+        } else if (holder is GPTViewHolder) {
+            holder.tvTitle.text = currentRoom.question
+            // Set other views specific to GPTViewHolder
+        }
     }
 
     fun updateRooms(newRooms: List<ChatClass>) {
